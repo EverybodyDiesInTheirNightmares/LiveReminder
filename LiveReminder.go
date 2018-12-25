cc9486a9256afdcca9cb71eb56a616fc979ec475
Eric000 <32562229+cs-zhuxudong@users.noreply.github.com>
2018-12-25 10:11:38 +0800

    Update LiveReminder.go

--- LiveReminder.go
+++ LiveReminder.go
@@ -3,45 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"net"
 	"net/http"
-	"net/smtp"
 	"os/exec"
 	"strings"
 	"time"
 )
 
 var TargetUrl = "https://www.douyu.com/606118"
-var chooice string
+var choice string
 
 func main() {
-	resp, err := http.Get(TargetUrl)
-	if err != nil {
-		// 异常
-	}
+	resp, _ := http.Get(TargetUrl)
 
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		// 异常
-	}
+	body, _ := ioutil.ReadAll(resp.Body)
 
-	//fmt.Println(string(body))
-	//fmt.Println(strings.Contains(string(body),"show_status = 2"))
 	if strings.Contains(string(body), "show_status = 1") == false {
 		fmt.Print("当前直播间未开播\n5秒后退出程序")
 		time.Sleep(5 * time.Second)
 	} else if strings.Contains(string(body), "show_status = 1") == true {
-		fmt.Print("大司马直播中\t请输入：\ngo直接进入捞马房间\t\tno关闭程序\n")
-		fmt.Scanf("%s", &chooice)
-		switch chooice {
+		fmt.Print("大司马直播中\t请输入：\ngo直接进入捞马房间\t\t任意键关闭程序\n")
+		fmt.Scanf("%s", &choice)
+		switch choice {
 		case "go":
 			cmd := exec.Command("cmd.exe", "/c", "start microsoft-edge:https://www.douyu.com/606118")
 			_ = cmd.Run()
-		case "no":
-
 		}
 
 	}
 }
-
